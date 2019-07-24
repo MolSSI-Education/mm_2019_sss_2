9a594b95e8da6b889863ae065c1bc0175bb1211a
Emmett Leddin <52934003+emleddin@users.noreply.github.com>
2019-07-24 16:16:14 -0500

    added the if statements for user-input files.

--- system.py
+++ system.py
@@ -4,17 +4,33 @@ import os
 
 class System:
 
-	def __init__(self, method='random', num_particles=None, box_length=None, filename=None):
+	def __init__(self, method='random', num_particles=None, box_length=None, \
+	 filename=None):
 		self.method = method
 
 		if self.method == 'random':
 			self.nparticles = num_particles
-			self.coordinates = (0.5 - np.random.rand(num_particles, 3)) * box_length
+			self.coordinates = (0.5 - np.random.rand(num_particles, 3)) * \
+			 box_length
 		elif self.method == 'file':
-			self.coordinates = np.loadtxt(filename, skiprows=2, usecols(1,2,3))
-			self.nparticles = len(self.coordinates)
-		else:
-			raise TypeError('You are using a method that is not supported at this moment.')
+			if os.path.exists(filename) == True:
+				self.coordinates = np.loadtxt(filename, skiprows=2, \
+				 usecols(1,2,3))
+				self.nparticles = len(self.coordinates)
+			elif:
+				print("Please enter the absolute path for your file.")
+				print("If you want to use something random, enter 'N'.")
+				filename = input("Enter path: ")
+				if filename == 'N' or 'n':
+					print("Will not use a file. Continuing.")
+					filename = None
+				else:
+					if os.path.exists(filename) == True:
+						continue
+					else:
+						print("Continuing without a file.")
+						filename = None
 
-		
-		
+		else:
+			raise TypeError('You are using a method that is not supported at \
+			 this moment.')
