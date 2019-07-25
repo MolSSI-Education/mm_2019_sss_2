9daeefe58ef5931369d674774250da4a8dd37f3f
Manav Kumar <makumar@bu.edu>
2019-07-24 19:18:55 -0500

    fixed some formatting post merge

--- system.py
+++ system.py
@@ -1,5 +1,4 @@
 import numpy as np
-import sys
 import os
 
 class System:
@@ -23,12 +22,10 @@ class System:
 
 		if self.method == 'random':
 			self.nparticles = num_particles
-			self.coordinates = (0.5 - np.random.rand(num_particles, 3)) * \
-			 box_length
+			self.coordinates = (0.5 - np.random.rand(num_particles, 3)) * box_length
 		elif self.method == 'file':
 			if os.path.exists(filename) == True:
-				self.coordinates = np.loadtxt(filename, skiprows=2, \
-				 usecols(1,2,3))
+				self.coordinates = np.loadtxt(filename, skiprows=2, usecols(1,2,3))
 				self.nparticles = len(self.coordinates)
 			elif:
 				print("Please enter the absolute path for your file.")
@@ -45,5 +42,4 @@ class System:
 						filename = None
 
 		else:
-			raise TypeError('You are using a method that is not supported at \
-			 this moment.')
+			raise TypeError('You are using a method that is not supported at  this moment.')
