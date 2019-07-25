75d6885881dbf0b80301903fb5e59de62b12bd21
Manav Kumar <makumar@bu.edu>
2019-07-25 06:03:40 -0500

    added a try statement.

--- system.py
+++ system.py
@@ -4,17 +4,17 @@ import os
 class System:
 	"""This is a class object that initializes a system for your Monte Carlo calculation.
 
-		Parameters
-		----------
-		method : str
-			The method is either 'random' or 'file'. By default the method is set to 'random'.
-		num_particles : int
-			The number of particles should be defined as an integer value. By default the value is 20.
-		box_length : float
-			The box is assumed to be cubic. By default the value is set to 3.0 Angstroms.
-		filename : str
-			The file name if the method is set to file.
-		"""
+	Parameters
+	----------
+	method : str
+		The method is either 'random' or 'file'. By default the method is set to 'random'.
+	num_particles : int
+		The number of particles should be defined as an integer value. By default the value is 20.
+	box_length : float
+		The box is assumed to be cubic. By default the value is set to 3.0 Angstroms.
+	filename : str
+		The file name if the method is set to file.
+	"""
 
 	def __init__(self, method='random', num_particles=20, box_length=3.0, filename=None):
 		self.box_length = box_length
@@ -23,25 +23,20 @@ class System:
 		if self.method == 'random':
 			self.n_particles = num_particles
 			self.coordinates = (0.5 - np.random.rand(num_particles, 3)) * box_length
+
 		elif self.method == 'file':
-			if os.path.exists(filename):
+			try:
 				self.coordinates, self.n_particles = self._read_info_from_file_(filename)
-			elif:
-				print("Please enter the absolute path for your file.")
-				print("If you want to use something random, enter 'N'.")
-				filename = input("Enter path: ")
-				if filename == 'N' or 'n':
-					print("Will not use a file. Continuing.")
-				else:
-					if os.path.exists(filename):
-						self.coordinates, self.n_particles = self._read_info_from_file_(filename)
-					else:
-						print("Continuing without a file.")
-						filename = None
+			except FileNotFoundError:
+				print('Incorrect file or the file was not found.')
+				print('Continuing with a random box for this Monte Carlo simulation')
+				self.n_particles = num_particles
+				self.coordinates = (0.5 - np.random.rand(num_particles, 3)) * box_length
+
 		else:
 			raise TypeError('You are using a method that is not supported at  this moment.')
 
-	def _read_info_from_file_(filename):
+	def _read_info_from_file_(self, filename):
 		coordinates = np.loadtxt(filename, skiprows=2, usecols=(1, 2, 3))
 		num_particles = len(coordinates)
 
